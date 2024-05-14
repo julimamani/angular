9aa2e4f26ff2ec3614bd7a0a8582b426305663af
julimamani <julietamamani47@gmail.com>
2024-05-14 15:53:04 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { Punto1Component } from './components/layout/punto1/punto1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Punto1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/layout/punto1/punto1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punto1',
+  templateUrl: './punto1.component.html',
+  styleUrl: './punto1.component.css'
+})
+export class Punto1Component {
+
+}
